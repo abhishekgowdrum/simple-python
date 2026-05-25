6bccf6eb6e12700dc921c7e969dec2482babca79
abhishekgowdrum <abhishekgowdru.m@gmail.com>
2026-05-25 19:51:08 +0530

    dockerimage created

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull abhishekgowdrum/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 abhishekgowdrum/simple-python-flask-app
