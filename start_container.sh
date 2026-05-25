e5a65a29bd36f6c74741c4d26085016e483a4d9e
abhishekgowdrum <abhishekgowdru.m@gmail.com>
2026-05-25 23:39:11 +0530

    con

--- start_container.sh
+++ start_container.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Stop and remove old container
 docker stop flask-app || true
 docker rm flask-app || true
 
-# Kill any process using port 5000
-sudo kill -9 $(sudo lsof -t -i:5000) || true
+# Kill process using port 5000
+PID=$(sudo lsof -t -i:5000)
 
-# Remove unused containers
+if [ ! -z "$PID" ]; then
+    sudo kill -9 $PID
+fi
+
+# Clean unused containers
 docker container prune -f || true
 
 # Pull latest image
 docker pull abhishekgowdrum/simple-python-flask-app:latest
 
-# Run new container
+# Run container
 docker run -d \
   --name flask-app \
   -p 5000:5000 \
